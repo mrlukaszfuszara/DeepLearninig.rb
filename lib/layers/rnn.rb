5e63d907febc1fd6b50ed4160cfc64175ca53a24
Lukasz Fuszara <mr.lukasz.fuszara@gmail.com>
2018-04-19 18:47:07 +0200

    0.0.11

--- lib/layers/rnn.rb
+++ lib/layers/rnn.rb
@@ -1,8 +1,9 @@
 require './lib/layers/rnn_layer'
 
 class RNN
-  def add_rnn(batch_size)
+  def add_rnn(batch_size, full_weights = false)
     @nn = RNNLayer.new(batch_size)
+    @full_weights = full_weights
   end
 
   def compile
@@ -10,10 +11,19 @@ class RNN
   end
 
   def fit(data_x, data_y, alpha)
-    i = 0
-    while i < data_x.size
-      tmp = @nn.fit_forward(data_x[i])
-      i += 1
+    if @full_weights
+      tmp = []
+      i = 0
+      while i < data_x.size
+        tmp[i] = @nn.fit_forward(data_x[i])
+        i += 1
+      end
+    else
+      i = 0
+      while i < data_x.size
+        tmp = @nn.fit_forward(data_x[i])
+        i += 1
+      end
     end
     tmp
   end
--- main.rb
+++ main.rb
@@ -7,38 +7,24 @@ class Main
   attr_reader :output
 
   def initialize(batch_size, data_x, data_y, epochs, alpha)
-# For Dense
-=begin
+    r = RNN.new
+    r.add_rnn(batch_size, true)
+    r.compile
+    rnn = r.fit(data_x, data_y, alpha)
     d1 = Dense.new
-    d1.add_dense(batch_size, 'sigmoid')
+    d1.add_dense(batch_size / 6, 'sigmoid')
     d1.add_dense(256, 'sigmoid')
     d1.add_dense(3, 'relu')
     d1.compile
-    @output = d1.fit(data_x, data_y, epochs, alpha)
-=end
-    r = RNN.new
-    r.add_rnn(batch_size)
-    r.compile
-    @output = r.fit(data_x, data_y, alpha)
+    @output = d1.fit(rnn, data_y, epochs, alpha)
   end
 end
-
-=begin
-f = Functions.new
-data_x = f.random_matrix_small(100, 50)
-data_y = [0.9, 0.4, 0.9]
-epochs = 100
-batch_size = 10
-alpha = 0.000001
-
-p Main.new(batch_size, data_x, data_y, epochs, alpha).output
-=end
 
 f = Functions.new
 data_x = f.random_matrix_small(100, 12)
 data_y = [0.9, 0.4, 0.9]
 epochs = 100
 batch_size = data_x[0].size
-alpha = 0.000001
+alpha = 0.00001
 
 p Main.new(batch_size, data_x, data_y, epochs, alpha).output
